6fdabbd3b1022dccf5f5d78778260dad0aa421b1
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-08-16 13:13:09 +0100

    display questions

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,22 @@ class Quizpage extends StatefulWidget {
 }
 
 class _QuizpageState extends State<Quizpage> {
+  List<Icon> scoreKeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    )
+  ];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +53,8 @@ class _QuizpageState extends State<Quizpage> {
           flex: 5,
           child: Center(
             child: Text(
-              'Are you sure',
+              questions[0],
+              textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white, fontSize: 25.0),
             ),
           ),
@@ -49,7 +66,16 @@ class _QuizpageState extends State<Quizpage> {
             child: FlatButton(
               focusColor: Colors.green.shade700,
               color: Colors.green,
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  scoreKeeper.add(
+                    Icon(
+                      Icons.access_alarm,
+                      color: Colors.white,
+                    ),
+                  );
+                });
+              },
               child: Text(
                 'True',
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
@@ -67,7 +93,11 @@ class _QuizpageState extends State<Quizpage> {
             child: FlatButton(
               focusColor: Colors.red.shade900,
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  scoreKeeper.removeLast();
+                });
+              },
               child: Text(
                 'False',
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
@@ -76,14 +106,41 @@ class _QuizpageState extends State<Quizpage> {
           ),
         ),
         Row(
-          children: [
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            )
-          ],
+          children: scoreKeeper,
         )
       ],
     );
   }
 }
+
+//question1: 'You can lead a cow down stairs but not up stairs.', false,
+//question2: 'Approximately one quarter of human bones are in the feet.', true,
+//question3: 'A slug\'s blood is green.', true,
+
+/*
+Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+ */
